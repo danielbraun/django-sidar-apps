8ec07d32c51d4201e032be0213353f040357823b
Daniel Braun <dbraun86@gmail.com>
2013-07-23 14:45:16 +0300

    Added free search to work filterings

--- backoffice/filters.py
+++ backoffice/filters.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 import django_filters
 from .models import Work, Subject, Collector
+from django.db.models import Q
+
+
+def free(qs, val):
+    return Work.objects.filter(
+        Q(designer__name_he__icontains=val) |
+        Q(designer__name_en__icontains=val) |
+        Q(category__name_he__icontains=val) |
+        Q(category__name_en__icontains=val) |
+        Q(subjects__name_he__icontains=val) |
+        Q(name_he__icontains=val)
+    )
 
 
 class WorkFilterSet(django_filters.FilterSet):
@@ -12,6 +24,11 @@ class WorkFilterSet(django_filters.FilterSet):
     of_collections = django_filters.ModelChoiceFilter(
         queryset=Collector.objects.all(),
         label=u'מאוסף'
+    )
+
+    q = django_filters.CharFilter(
+        label=u'חיפוש חופשי',
+        action=free
     )
 
     class Meta:
